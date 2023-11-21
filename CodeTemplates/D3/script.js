06928cae361bd1af1eed0908e5ec0c0091b02c9c
bsilver3 <105662471+bsilver3@users.noreply.github.com>
2023-11-21 10:33:33 -0700

    D3 Stuff

--- CodeTemplates/D3/script.js
+++ CodeTemplates/D3/script.js
@@ -3,5 +3,5 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: [-122.33, 47.60], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 10 // starting zoom
 });
